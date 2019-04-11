24a528eaf2ba856caaad946684098c8f525cacfb
Nicholas Clarke <nick@topos.org.uk>
2019-04-11 09:35:34 +0200

    Fix: Disable doc-view-display-toggle.
    For some reason this is not advised.

--- init.el
+++ init.el
@@ -128,5 +128,5 @@ Check `dotspacemacs/get-variable-string-list' for all vars you can configure."
   (dotspacemacs/user-config/post-layer-load-config)
 
   ;; Drop-in whatever config here without having to worry about good practices
-  (ad-deactivate 'doc-view-toggle-display)
+  ;; (ad-deactivate 'doc-view-toggle-display)
   )
